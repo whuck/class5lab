3e344583e40ce5cb2890a55ec548cad92d51dcaa
BIT student <bitstudent@bitnet.wctc.edu>
2020-11-03 15:10:00 -0600

    done

--- code.js
+++ code.js
@@ -6,19 +6,28 @@ $(document).ready(function()
         var rank = $("input:checked").attr('value');
         var name = $("#fname").val() + " " + $("#lname").val();
         var greeting = `Hello ${rank} ${name}!`;
+        var textColor = "";
+        switch(rank) {
+            case "Private" : textColor="grey";break;
+            case "PFC" : textColor="pink";break;
+            case "Specialist" : textColor="blue";break;
+            case "Sgt." : textColor="red";break;
+        }
         $("#greeting").text(greeting);
+        $("#greeting").css('color',textColor);
     }
 
     $("#qty1").change(updateTotal);
+    $("#qty2").change(updateTotal);
     function updateTotal(){
-        var subTotal = parseFloat($("#qty1").val()) * $("#price1").data('price');
-        //alert(subTotal);
-        $("#subtotal1").val(subTotal);
+        var row1subTotal = parseFloat($("#qty1").val()) * $("#price1").data('price');
+        var row2subTotal = parseFloat($("#qty2").val()) * $("#price2").data('price');
+
+        $("#subtotal1").text(row1subTotal);
+        $("#subtotal2").text(row2subTotal);
+
+        $("#total").text(row1subTotal+row2subTotal);
 
     }
-    $(".nav-link").click(showTab);
-    function showTab(e) {
-        e.preventDefault();
-        $(this).tab("show");
-    }
+
 });
